3841760e7022b5b2dc28758f8ab528709f7c288b
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-21 11:32:49 +0100

    Add data object with map to only save existing phones

---
+++ data.js
@@ -0,0 +1,7 @@
+let data = [
+  { phone: 'iPhone X 64GB', premium: 50.42 },
+  { phone: 'Google Pixel 2 64GB', premium: 40 },
+  { phone: "Samsung Galaxy S9", premium: 45.99 }
+]
+
+module.exports = data
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 let bodyParser = require('body-parser')
 let Transaction = require('./app/models/transaction')
+let data = require('./data')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -9,12 +10,18 @@ let mongoose = require('mongoose')
 mongoose.connect("mongodb://localhost:27017/BrollyAPI")
 
 app.post('/transaction', (req, res) => {
+  console.log(req.body)
   let newTransaction = new Transaction(req.body)
-  newTransaction.save().then((transaction) => {
-    res.json ({ message: 'Success!', transaction})
-  }).catch((err) => {
-    res.status(400).send(err)
+  data.map((element) => {
+    if(element.phone == req.body.device) {
+      newTransaction.save().then((transaction) => {
+        res.json ({ message: 'Success!', transaction})
+      }).catch((err) => {
+        res.status(400).send(err)
+      })
+    }
   })
+
 })
 
 
